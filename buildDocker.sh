39a7be6d80bac91c727c4510b9f8f57840fb0102
S Das <sibendu.das@gmail.com>
2022-01-17 17:01:58 +0000

    Update buildDocker.sh

--- buildDocker.sh
+++ buildDocker.sh
@@ -3,6 +3,8 @@ cd coms-ui
 docker build -t sibendu/coms-ui .
 docker push sibendu/coms-ui 
 
+cd ..
+
 cd coms-customer-api
 docker build -t sibendu/coms-customer-api .
 docker push sibendu/coms-customer-api
