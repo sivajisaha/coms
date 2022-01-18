b39f9c80692115a000fd6f24edfd85221e09dd24
Snehashish Chanda <snehashish.learning@gmail.com>
2022-01-18 06:52:15 +0000

    Update buildDocker.sh

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,9 +1,9 @@
 cd coms-ui
-docker build -t sibendu/coms-ui .
-docker push sibendu/coms-ui 
+docker build -t registry.gitlab.com/jucoms/coms/coms-ui .
+docker push registry.gitlab.com/jucoms/coms/coms-ui 
 
 cd ..
 
 cd coms-customer-api
-docker build -t sibendu/coms-customer-api .
-docker push sibendu/coms-customer-api
+docker build -t registry.gitlab.com/jucoms/coms/coms-customer-api .
+docker push registry.gitlab.com/jucoms/coms/coms-customer-api
